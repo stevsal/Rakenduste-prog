7a8c7fa432bdebdf4f999428f742ed1c4684c786
stevsal <ssaluri@tlu.ee>
2019-09-08 18:08:35 +0300

    Heroku test

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require("path");
-const port = 3000;
+const PORT = 3000;
 
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, "static", "index.html"));
@@ -9,5 +9,6 @@ app.get('/', (req, res) => {
 
 app.use(express.static('static'))
 
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(process.env.PORT || PORT, () => {
+  console.log("Server started", PORT);
+});
