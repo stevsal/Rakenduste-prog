6b703c2ca983b13ae99bd8336e5ed05940ef4b18
stevsal <ssaluri@tlu.ee>
2019-09-09 14:40:57 +0300

    warmup done

--- static/myScript.js
+++ static/myScript.js
@@ -3,6 +3,10 @@ window.onload = (event) => {
   alert("Hello");
 };
 
+/*const x = window.location;
+console.log(x);
+const urlParams = new URLSearchParams(window.location.search);
+console.log(urlParams.getAll());*/
 
 document.getElementsByClassName('logo').addEventListener('click', toHomepage());
 
